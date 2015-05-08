020c035a74b2cb884658fb29a9e1e007d3f6d95f
tammspx <tam.mspx@gmail.com>
2015-05-07 20:39:18 -0700

    Coded Java

--- mouse_a_sketch/base.js
+++ mouse_a_sketch/base.js
@@ -2,4 +2,11 @@ console.log("this is a sanity check");
 
 $(document).ready(function(){
   console.log("Let's get coding!")
+
+  var color = $("input");
+
+	$('td').hover(function(){
+	   $(this).css("background",color.val());
+	});
+
 })
